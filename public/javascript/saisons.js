a1cae6ec90e4e09c2d02610c0ba92873e650f13b
romdk <drack.roman67@gmail.com>
2023-02-24 15:54:01 +0100

    afficher calendrier et classements saison

--- public/javascript/saisons.js
+++ public/javascript/saisons.js
@@ -2,6 +2,8 @@
     const currentYear = new Date().getFullYear()
     const saison = document.getElementById('saison')
     const select = document.getElementById('saisonSelect')
+    const calendrier = document.getElementById('calendrier')
+    const classements = document.getElementById('classements')
 
     var couleursEcuries = {
         red_bull : '#0d1a2a',
@@ -24,10 +26,8 @@
         select.append(option)
     }
 
-    // par défaut year = l'année actuelle
+    // on met l'année en cours par défaut
     let year = currentYear
-    // par défaut on affiche le classement pilote
-    let show = 'drivers'
 
     function affElements(){
 
@@ -43,9 +43,11 @@
         removeIfExists('constructorStandingsUl')
         removeIfExists('standingsSelector')
         
+        // on affiche la liste des gp seulement pour l'année en cours
         if (year == currentYear){
+            calendrier.style.display = 'flex'
 
-            // recupere la liste des GP
+            // recupere la liste des GP depuis l'api
             const url = '/grandsprix'
             fetch(url , {
                 method: 'POST',
@@ -61,7 +63,7 @@
                 const rep = await response.json()
                 const listeGp = document.createElement('ul')              
 
-                saison.appendChild(listeGp)
+                calendrier.appendChild(listeGp)
                 listeGp.id = 'listeGp'
                 listeGp.classList.add('listeGp')
 
@@ -80,151 +82,160 @@
                     dateGp.innerHTML = moment(race.date).format('D MMMM')
                 })
             })
+        }else {
+            calendrier.style.display = 'none'
         }
 
-        if (year != currentYear){
-            // On créer un selecteur pour choisir d'afficher le classement pilotes ou constructeurs
-            const standingsSelector = document.createElement('div')
-            saison.appendChild(standingsSelector)
-            standingsSelector.id = 'standingsSelector'
-            
-            const driversBtn = document.createElement('button')
-            standingsSelector.appendChild(driversBtn)
-            driversBtn.innerHTML = 'Pilotes'
-            
-            const constructorsBtn = document.createElement('button')
-            standingsSelector.appendChild(constructorsBtn)
-            constructorsBtn.innerHTML = 'Constructeurs'  
-
-            driversBtn.addEventListener('click', () => {
-                show = 'drivers'
-                driversBtn.style.color = "#f1f1f1"
-                constructorsBtn.style.color = "#8d8d8d"
-                affElements()
+        // On créer un selecteur pour choisir d'afficher le classement pilotes ou constructeurs
+        const standingsSelector = document.createElement('div')
+        classements.appendChild(standingsSelector)
+        standingsSelector.id = 'standingsSelector'
+        
+        let driversBtn = document.createElement('button')
+        standingsSelector.appendChild(driversBtn)
+        driversBtn.innerHTML = 'Pilotes'
+        driversBtn.id = 'driversBtn'
+        driversBtn.style.color = '#f1f1f1'
+        
+        let constructorsBtn = document.createElement('button')
+        standingsSelector.appendChild(constructorsBtn)
+        constructorsBtn.innerHTML = 'Constructeurs'
+        constructorsBtn.id = 'constructorsBtn'          
+        
+        // récupere le classement pilotes depuis l'api
+        const urlDriverStandings = '/driverStandings'
+        fetch(urlDriverStandings, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded',
+                'Accept': '*/*'
+            },
+            body: new URLSearchParams({
+                'year' : year
             })
-            
-            constructorsBtn.addEventListener('click', () => {
-                show = 'constructors'
-                driversBtn.style.color = "#8d8d8d"
-                constructorsBtn.style.color = "#f1f1f1"
-                affElements()
+        })
+        .then(async (response) => {
+            const rep = await response.json()
+            const driverStandings = rep.MRData.StandingsTable.StandingsLists[0].DriverStandings
+            const driverStandingsUl = document.createElement("ul")
+            driverStandingsUl.id = 'driverStandingsUl'                   
+
+            classements.appendChild(driverStandingsUl)
+            driverStandingsUl.id = 'driverStandingsUl'
+            driverStandingsUl.classList.add('classement')
+
+            // pour chaque résultat on ajoute une ligne dans notre liste driverStandings
+            driverStandings.forEach(result => {
+                const driverStandingsLi = document.createElement("li")
+                driverStandingsUl.appendChild(driverStandingsLi)
+
+                const position = document.createElement("span")
+                driverStandingsLi.appendChild(position)
+                position.innerHTML = result.position
+
+                const couleur = document.createElement("div")                        
+                driverStandingsLi.appendChild(couleur)
+                // !!API pas a jour pour 2023!!
+                if (year == currentYear){
+                    couleur.style.backgroundColor ="black"
+                }else{
+                    couleur.style.backgroundColor = couleursEcuries[result.Constructors[0].constructorId]
+                }
+
+                const prenom = document.createElement("span")
+                driverStandingsLi.appendChild(prenom)
+                prenom.innerHTML = result.Driver.givenName
+
+                const nom = document.createElement("span")
+                driverStandingsLi.appendChild(nom)
+                nom.innerHTML = result.Driver.familyName
+
+                const ecurie = document.createElement("span")
+                driverStandingsLi.appendChild(ecurie)
+                // !!API pas a jour pour 2023!!
+                if (year == currentYear){
+                    ecurie.innerHTML = 'undefined'
+                } else {
+                    ecurie.innerHTML = result.Constructors[0].name
+                }
+
+                const arrow = document.createElement("span")
+                driverStandingsLi.appendChild(arrow)
+                arrow.innerHTML = '<i class="fa-solid fa-chevron-right"></i>'            
+            });
+        })
+
+        // récupere le classement constructeur depuis l'api
+        const urlConstructorStandings = '/constructorStandings'
+        fetch(urlConstructorStandings, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded',
+                'Accept': '*/*'
+            },
+            body: new URLSearchParams({
+                'year' : year
             })
+        })
+        .then(async (response) => {
+            const rep = await response.json()
+            const constructorStandings = rep.MRData.StandingsTable.StandingsLists[0].ConstructorStandings
+            const constructorStandingsUl = document.createElement("ul")
             
-            
-            if (show == 'drivers') {
-                // récupere le classement pilotes
-                const urlDriverStandings = '/driverStandings'
-                fetch(urlDriverStandings, {
-                    method: 'POST',
-                    headers: {
-                        'Content-type': 'application/x-www-form-urlencoded',
-                        'Accept': '*/*'
-                    },
-                    body: new URLSearchParams({
-                        'year' : year
-                    })
-                })
-                .then(async (response) => {
-                    const rep = await response.json()
-                    const driverStandings = rep.MRData.StandingsTable.StandingsLists[0].DriverStandings
-                    const driverStandingsUl = document.createElement("ul")
-                    driverStandingsUl.id = 'driverStandingsUl'                   
+            classements.appendChild(constructorStandingsUl)
+            constructorStandingsUl.id = 'constructorStandingsUl'
+            constructorStandingsUl.classList.add('classement')
+            constructorStandingsUl.style.display = "none"
 
-                    saison.appendChild(driverStandingsUl)
-                    driverStandingsUl.id = 'driverStandingsUl'
-                    driverStandingsUl.classList.add('classement')
+            // pour chaque résultat on ajoute une ligne dans notre liste constructorStandings
+            constructorStandings.forEach(result => {
+                const constructorStandingsLi = document.createElement("li")
+                constructorStandingsUl.appendChild(constructorStandingsLi)
 
-                    // pour chaque résultat on ajoute une ligne dans notre liste driverStandings
-                    driverStandings.forEach(result => {
-                        const driverStandingsLi = document.createElement("li")
-                        driverStandingsUl.appendChild(driverStandingsLi)
+                const position = document.createElement("span")
+                constructorStandingsLi.appendChild(position)
+                position.innerHTML = result.position
 
-                        const position = document.createElement("span")
-                        driverStandingsLi.appendChild(position)
-                        position.innerHTML = result.position
+                const couleur = document.createElement("div")
+                constructorStandingsLi.appendChild(couleur)
+                couleur.style.backgroundColor = couleursEcuries[result.Constructor.constructorId]      
 
-                        const couleur = document.createElement("div")                        
-                        driverStandingsLi.appendChild(couleur)
-                        couleur.style.backgroundColor = couleursEcuries[result.Constructors[0].constructorId]
+                const ecurie = document.createElement("span")
+                constructorStandingsLi.appendChild(ecurie)
+                ecurie.innerHTML = result.Constructor.name
 
-                        const prenom = document.createElement("span")
-                        driverStandingsLi.appendChild(prenom)
-                        prenom.innerHTML = result.Driver.givenName
-
-                        const nom = document.createElement("span")
-                        driverStandingsLi.appendChild(nom)
-                        nom.innerHTML = result.Driver.familyName
-
-                        const ecurie = document.createElement("span")
-                        driverStandingsLi.appendChild(ecurie)
-                        ecurie.innerHTML = result.Constructors[0].name
-
-                        const arrow = document.createElement("span")
-                        driverStandingsLi.appendChild(arrow)
-                        arrow.innerHTML = '<i class="fa-solid fa-chevron-right"></i>'            
-                    });
-
-
-                })
-            }else if(show == 'constructors'){
-                console.log('test');
-                // récupere le classement constructeur
-                const urlConstructorStandings = '/constructorStandings'
-                fetch(urlConstructorStandings, {
-                    method: 'POST',
-                    headers: {
-                        'Content-type': 'application/x-www-form-urlencoded',
-                        'Accept': '*/*'
-                    },
-                    body: new URLSearchParams({
-                        'year' : year
-                    })
-                })
-                .then(async (response) => {
-                    const rep = await response.json()
-                    const constructorStandings = rep.MRData.StandingsTable.StandingsLists[0].ConstructorStandings
-                    const constructorStandingsUl = document.createElement("ul")
-                    
-                    saison.appendChild(constructorStandingsUl)
-                    constructorStandingsUl.id = 'constructorStandingsUl'
-                    constructorStandingsUl.classList.add('classement')
-    
-                    // pour chaque résultat on ajoute une ligne dans notre liste constructorStandings
-                    constructorStandings.forEach(result => {
-                        const constructorStandingsLi = document.createElement("li")
-                        constructorStandingsUl.appendChild(constructorStandingsLi)
-
-                        const position = document.createElement("span")
-                        constructorStandingsLi.appendChild(position)
-                        position.innerHTML = result.position
-
-                        const couleur = document.createElement("div")
-                        constructorStandingsLi.appendChild(couleur)
-                        couleur.style.backgroundColor = couleursEcuries[result.Constructor.constructorId]      
-
-                        const ecurie = document.createElement("span")
-                        constructorStandingsLi.appendChild(ecurie)
-                        ecurie.innerHTML = result.Constructor.name
-
-                        const arrow = document.createElement("span")
-                        constructorStandingsLi.appendChild(arrow)
-                        arrow.innerHTML = '<i class="fa-solid fa-chevron-right"></i>'            
-                    });   
-                })
-            }
-
-
-        }
+                const arrow = document.createElement("span")
+                constructorStandingsLi.appendChild(arrow)
+                arrow.innerHTML = '<i class="fa-solid fa-chevron-right"></i>'            
+            });   
+        })
+        // on affiche le tableau pilotes lorsqu'on clique sur le bouton pilotes
+        driversBtn.addEventListener('click', () => {        
+            driversBtn.style.color = "#f1f1f1"
+            constructorsBtn.style.color = "#8d8d8d"
+            driverStandingsUl.style.display = "block"
+            constructorStandingsUl.style.display = "none"
+        })
+        // on affiche le tableau constructeurs lorsqu'on clique sur le bouton constructeurs
+        constructorsBtn.addEventListener('click', () => {
+            driversBtn.style.color = "#8d8d8d"
+            constructorsBtn.style.color = "#f1f1f1"
+            driverStandingsUl.style.display = "none"
+            constructorStandingsUl.style.display = "block"
+        })
     }
     
-    affElements()
-    
+    affElements()   
+
     // l'orsqu'on change l'année du selecteur, on execute la fonction affElements()
     select.addEventListener('change', (e) => {        
         e.preventDefault()
         year = select.selectedOptions[0].value   
         affElements()     
     })
+
+
+
 
 
     
