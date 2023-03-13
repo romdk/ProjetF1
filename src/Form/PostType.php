da637d337c9ac29b955a557ac53efbf5294bc532
romdk <drack.roman67@gmail.com>
2023-03-13 09:54:38 +0100

    modif formulaire message

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -9,28 +9,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;   
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+// use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('message', TextType::class, [
-                'attr' => ['class' => 'messageBox'] 
+            ->add('message', TextareaType::class, [
+                'label' => false,
+                'attr' => ['class' => 'messageBox','Maxlength' => 255,]
             ])
-            // ->add('dateCreation', DateTimeType::class)
-            // ->add('user', EntityType::class, [
-            //     'class' => User::class,
-            //     'choice_label' => 'id'
-            // ])
-            // ->add('grandprix', EntityType::class, [
-            //     'class' => Grandprix::class,
-            //     'choice_label' => 'id'
-            // ])
+
             ->add('ajouter', SubmitType::class, [
+                'label' => 'Commenter',
                 'attr' => ['class' => 'btnAjouter']
             ])
         ;
