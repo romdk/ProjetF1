093a606e2efcfd10511d0a77a55b600cbd791aea
romdk <drack.roman67@gmail.com>
2023-03-14 16:06:41 +0100

    creer reservation

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Circuit;
+use App\Entity\Grandprix;
+use App\Entity\Emplacement;
+use App\Entity\Reservation;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,16 +16,41 @@ class ReservationController extends AbstractController
 {
 
     
-    // #[Route('/reservation/{id}', name: 'app_reservation')]
-    // public function index(Request $request, Circuit $circuit): Response
-    // {
-    //     $id = $request->attributes->get('_route_params');
-    //     // $emplacements = $circuit->getEmplacements();
+    #[Route('/grandprix/{id}/reservationCreate', name: 'creer_reservation')]
+    public function creerReservation(ManagerRegistry $doctrine, $id)
+    { 
+        if($this->getUser()){       
+            if(isset($_POST['session']) && isset($_POST['emplacement']) && isset($_POST['nbPersonnes'])){
+                // on récupère les données du formulaire
+                $session =filter_input(INPUT_POST,"session",FILTER_SANITIZE_STRING);
+                $emplacementId =filter_input(INPUT_POST,"emplacement",FILTER_VALIDATE_INT);
+                $nbPersonnes =filter_input(INPUT_POST,"nbPersonnes",FILTER_VALIDATE_INT);
 
-    //     return $this->render('reservation/index.html.twig', [
-    //         'controller_name' => 'ReservationController',
-    //         'route_param' => $id,
-    //         // 'emplacements' => $emplacements,
-    //     ]);
-    // }
+                $entityManager = $doctrine->getManager();
+                // on récupère l'année depuis l'id de la route
+                $year = substr($id,0,4);
+                // on récupère le round depuis l'id de la route
+                $round = substr($id,5,7);
+                // on récupère le grand prix grace à l'année et le round
+                $grandprix = $doctrine->getRepository(Grandprix::class)->findOneBy(['season' => $year, 'round' => $round],[]);
+                // on récupère l'emplacement avec l'id emplacement du formulaire
+                $emplacement = $doctrine->getRepository(Emplacement::class)->find($emplacementId);
+
+                // on créer une nouvelle réservation et on lui attribue les différentes données
+                $reservation = new Reservation();
+                $reservation->setUser($this->getUser());
+                $reservation->setGrandprix($grandprix);
+                $reservation->setSession($session);
+                $reservation->setEmplacement($emplacement);
+                $reservation->setNbPersonnes($nbPersonnes);
+                $entityManager->persist($reservation);
+                $entityManager->flush();
+                return $this->redirectToRoute('app_grandprix', ['id' => $id]);
+            } else {
+                dd('erreur');
+                }
+        } else {
+            return $this->redirectToRoute('app_login');
+            }
+    }
 }
