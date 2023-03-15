70d3102af8fd81150e3bdeaccccc888732b237e7
romdk <drack.roman67@gmail.com>
2023-03-15 13:56:27 +0100

    style formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,22 +4,32 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('pseudonyme',TextType::class, [
+                'label' => false,
+                'attr' => ['class' => 'email', 'placeholder' => "Saisir un nom d'utilisateur"]
+            ])
+            ->add('email',EmailType::class, [
+                'label' => false,
+                'attr' => ['class' => 'email', 'placeholder' => 'Saisir une adresse mail']
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => 'Accepter les Termes et Conditions',
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -29,9 +39,10 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => false,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
+                'attr' => ['autocomplete' => 'new-password','class' => 'password', 'placeholder' => 'Saisir un mot de passe'],
+                'constraints' => [  
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
