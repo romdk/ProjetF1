63b60748e465a7fdd7fc0d093c52d619075ff4c6
romdk <drack.roman67@gmail.com>
2023-04-04 11:54:17 +0200

    articles favoris

--- src/Controller/ActuController.php
+++ src/Controller/ActuController.php
@@ -12,11 +12,42 @@ class ActuController extends AbstractController
     #[Route('/actu', name: 'app_actu')]
     public function index(ActuHttpClient $actu): Response
     {
+        // recupere tous les articles
         $dataArticles = $actu->getArticles();
         $articles = json_decode($dataArticles,true);
-        // dd($articles);
+
+        $articlesFav = array();
+        
+        if($this->getUser()){
+            // créer un tableau des favoris de l'utilisateur
+            $favoris = array();
+            
+            // ajoute les pilotes favoris de l'utilisateur au tableau favoris
+            $pilotesFav = $this->getUser()->getPilotes();
+            foreach ( $pilotesFav as $pilote){
+                $dataArticles = $actu->getArticlesByDriver($pilote->getIdApi());
+                $articlesPilote = json_decode($dataArticles,true);
+                $favoris[] = $articlesPilote;
+            }
+    
+            // ajoute les ecuries favorites de l'utilisateur au tableau favoris
+            $ecuriesFav = $this->getUser()->getEcuries();
+            foreach ( $ecuriesFav as $ecurie){
+                $dataArticles = $actu->getArticlesByConstructor($ecurie->getIdApi());
+                $articlesEcurie = json_decode($dataArticles,true);
+                $favoris[] = $articlesEcurie; 
+            }
+            
+            // pour chaque favori on ajoute ses articles au tableau d'articles
+            foreach($favoris as $favori){
+                foreach($favori['hydra:member'] as $article)
+                $articlesFav[] = $article;
+            }
+        }
+
         return $this->render('actu/index.html.twig', [
             'articles' => $articles,
+            'articlesFav' => $articlesFav,
         ]);
     }
 }
