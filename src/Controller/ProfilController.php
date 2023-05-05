8799ffe92f965e4d71636f848d356c986ec03497
romdk <drack.roman67@gmail.com>
2023-05-05 19:36:27 +0200

    edit profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\HttpClient\F1HttpClient;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
-    public function index(F1HttpClient $f1): Response
+    #[Route('/profil/{id}', name: 'app_profil')]
+    public function index(F1HttpClient $f1, ManagerRegistry $doctrine, User $user, Request $request, SluggerInterface $slugger, $id): Response
     {
+        $currUserId = $this->getUser()->getId();
+        if($currUserId != $id){
+            return $this->redirectToRoute('app_profil', ['id' => $currUserId]);
+            // dd($this->getUser()->getId());
+        }
         // on récupère les reservations de l'utilisateur connecté
         $reservations = $this->getUser()->getReservations();
         // on créer un tableau associatif
@@ -24,12 +34,49 @@ class ProfilController extends AbstractController
             // puis on récupère les données du grandprix depuis l'api
             $data = $f1->getDetailsGrandprix($year, $round);
             // on remplis le tableau associatif avec comme clé l'id de la réservation et comme valeur le nom du grandprix
-            $grandprix[$reservation->getId()] = json_decode($data,true)["MRData"]["RaceTable"]["Races"]["0"]["raceName"];
+            $grandprix[$reservation->getId()] = ["raceName" => json_decode($data,true)["MRData"]["RaceTable"]["Races"]["0"]["raceName"], "date" =>json_decode($data,true)["MRData"]["RaceTable"]["Races"]["0"]["date"]];
         }
+
+        $form = $this->createForm(UserType::class, $user);
+            $form->handleRequest($request);
+
+            $file = $form->get('image')->getData();
+
+            if ($file) {
+                // recupere le nom du fichier original
+                $originalFileName = pathinfo($file->getClientOriginalName(), flags:PATHINFO_FILENAME);
+                // trasforme le nom du fichier en une string qui contient que des caracteres ASCII
+                $safeFileName = $slugger->slug($originalFileName);
+                // ajout un id unique au fichier pour eviter les doublons
+                $newFileName = $safeFileName.'-'.uniqid().'.'.$file->guessExtension();
+
+                // deplace le fichier uploader dans le bon dossier
+                try{
+                    $file->move(
+                        $this->getParameter(name:'user_directory'),
+                        $newFileName
+                    );
+                } catch (FileExeption $e) {
+
+                }
+
+                $user->setImage($newFileName);
+            }
+
+            if($form->isSubmitted() && $form->isValid())
+            {
+                $user = $form->getData();
+                $entityManager = $doctrine->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_profil', ['id' => $user->getId()]);
+            }
 
         return $this->render('profil/index.html.twig', [
             'reservations' => $reservations,
             'grandprix' => $grandprix,
+            'formEditUser' => $form->createView()
         ]);
     }
 }   
---
+++ src/Form/UserType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            // ->add('email')
+            // ->add('roles')
+            // ->add('password')
+            // ->add('isVerified')
+            ->add('pseudonyme', TextType::class)
+            ->add('image', FileType::class, [
+                'label' => 'image de profil',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Upload un fichier valide'
+                    ])
+                ],
+            ])
+            ->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
