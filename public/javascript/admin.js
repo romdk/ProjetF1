c8829ac9c41966e5c4f5d22305b4561fa8060508
romdk <drack.roman67@gmail.com>
2023-05-26 17:10:11 +0200

    aff users & messages, ban/unban & suppr message

---
+++ public/javascript/admin.js
@@ -0,0 +1,61 @@
+// -------------------------------------------------LISTE UTILISATEURS---------------------------------------------
+// let selectedUser = selectedUser
+// on selectionne tous les utilisateurs
+let messages = document.querySelector('#listeMessages')
+document.querySelectorAll('.user').forEach(element => {
+    element.addEventListener('click', () => {
+        console.log(element.dataset.user);
+        console.log(messages);
+        // selectedUser = element.
+    })
+})
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------BOUTON BANNIR---------------------------------------------
+// on selectione tous les boutons
+document.querySelectorAll('[data-ban]').forEach(element => {
+
+    // on selectionne le curseur du bouton
+    let curseur = element.querySelector('.toggle')
+    // on selectionne le texte du bouton
+    let texte = curseur.querySelector('.text')
+
+    // event listener sur chaque bouton
+    element.addEventListener('click', function(event){
+        event.preventDefault()
+        const url = this.href
+        console.log(url)
+        fetch(url)
+
+        // on ajoute/enleve la classe activeToggle à chaque click
+        curseur.classList.toggle('activeToggle')
+
+        // on remplace le texte ON/OFF
+        if (texte.innerHTML === 'OFF'){
+            texte.innerHTML = 'ON'
+        }else{
+            texte.innerHTML = 'OFF'
+        }
+    })
+})  
+
+// -------------------------------------------------BOUTON BANNIR---------------------------------------------
+// on selectionne tous les bouton delete
+document.querySelectorAll('[data-delete]').forEach(element => {
+    // event listener sur chaque bouton
+    element.addEventListener('click', function(event){
+        event.preventDefault();
+        const url = this.href
+        console.log(url);
+        fetch(url)
+        element.parentNode.remove()
+    })
+})
+
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Entity\User;
+use App\Entity\Reponse;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,34 +14,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(Security $security, ManagerRegistry $doctrine): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        if ($security->isGranted('ROLE_ADMIN')) {
+            $users = $doctrine->getRepository(User::class)->findAll();
+            $posts = $doctrine->getRepository(Post::class)->findAll();
+            $reponses = $doctrine->getRepository(Reponse::class)->findAll();
+            
+            return $this->render('admin/index.html.twig', [
+                'users' => $users,
+                'posts' => $posts,
+                'reponses' => $reponses,
+            ]);
+        } else {
+            return $this->redirectToRoute('app_home');
+        }
+        
     }
 
     #[Route('/user/{id}/ban', name: 'ban_user')]
     public function banUser(ManagerRegistry $doctrine, User $user, Security $security)
     { 
         if($security->isGranted('ROLE_ADMIN')){ 
-            $user->setStatut(1);
             $entityManager = $doctrine->getManager();
-            $entityManager->persist($user);
 
-            $reponses = $user->getReponses();
-            foreach ($reponses as $reponse){
-                $entityManager->remove($reponse);
+            if($user->isStatut() == 0){
+                $user->setStatut(1);
+    
+                $reponses = $user->getReponses();
+                foreach ($reponses as $reponse){
+                    $entityManager->remove($reponse);
+                }
+    
+                $posts = $user->getPosts();
+                foreach ($posts as $post){
+                    $post->setStatut(1);
+                    $entityManager->persist($post);
+                }
             }
-
-            $posts = $user->getPosts();
-            foreach ($posts as $post){
-                $post->setStatut(1);
-                $entityManager->persist($post);
+            elseif($user->isStatut() == 1){
+                $user->setStatut(0);
             }
             
+            $entityManager->persist($user);
             $entityManager->flush();
-            return $this->redirectToRoute('app_home');        
+            return $this->redirectToRoute('app_admin');        
         }else {
             return $this->redirectToRoute('app_home');
             }
