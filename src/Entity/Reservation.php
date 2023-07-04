fd9ad24a3f3a56ac9982fe388eaf4a4783902411
romdk <drack.roman67@gmail.com>
2023-07-04 16:49:59 +0200

    use dateTime

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -2,9 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\ReservationRepository;
+use DateTime;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ReservationRepository;
 
 #[ORM\Entity(repositoryClass: ReservationRepository::class)]
 class Reservation
